17df085567567277d42f87b49a573267b174118d
Vishnu V K <vishnugb@gmail.com>
2019-02-16 10:58:13 +0530

    begin p5

---
+++ ProjectEuler/p5_smallest_multiple.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+#   Copyright (C) 2019  Vishnu V. Krishnan : vishnugb@gmail.com
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""
+Smallest multiple
+
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+from time import process_time
+
+START = process_time()
+
+FINISH = process_time()
+print(f"{finish-START:.5f} seconds")
