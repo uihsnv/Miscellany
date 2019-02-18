ee937c27fe5e3b89ded10594dabffc0b6651d12f
Vishnu V. Krishnan <vishnugb@gmail.com>
2019-02-18 13:22:43 +0530

    style changes to p6

--- ProjectEuler/p6_sum_square_difference.py
+++ ProjectEuler/p6_sum_square_difference.py
@@ -26,9 +26,11 @@ Sum square difference
 
                 (1 + 2 + ... + 10)^2 = 552 = 3025
 
-    Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+    Hence the difference between the sum of the squares of the first ten
+    natural numbers and the square of the sum is 3025 − 385 = 2640.
 
-    Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    Find the difference between the sum of the squares of the first one hundred
+    natural numbers and the square of the sum.
 """
 
 from time import process_time
